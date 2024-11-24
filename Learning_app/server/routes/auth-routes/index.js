1ce0fa46be4eaefe5a9ef440e765a540ba0026b0
practice-repo <amangupta9039093489@gmail.com>
2024-11-24 10:00:12 +0530

    Resolved merge conflict in auth-route

--- Learning_app/server/routes/auth-routes/index.js
+++ Learning_app/server/routes/auth-routes/index.js
@@ -8,16 +8,17 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-<<<<<<< HEAD
-router.get("/check-auth",authenticateMiddleware,(req,res) => {
-=======
-router.post("/check-auth",authenticateMiddleware,(req,res) => {
->>>>>>> 23a8e5a342886d56f4671f89873239e6198dca7a
-    const user = req.user
+
+router.get("/check-auth", authenticateMiddleware, (req, res) => {
+ 
+    const user = req.user;
     res.status(200).json({
-        success : true,
-        message : 'Authenticated user!'
-    })
-})
+      success: true,
+      message: "Authenticated user!",
+      data : {
+        user,
+      }
+    });
+  });
 
 module.exports = router;
