1737e5d5f70954469cd1c9832878be61313f73ca
practice-repo <amangupta9039093489@gmail.com>
2024-11-27 19:28:51 +0530

    add RouteGuard

--- Learning_app/client/src/components/RouteGuard/index.jsx
+++ Learning_app/client/src/components/RouteGuard/index.jsx
@@ -1,4 +1,3 @@
-const { Navigation } = require("lucide-react");
 const { Fragment } = require("react");
 const { useLocation, Navigate } = require("react-router-dom");
 
@@ -11,11 +10,11 @@ function RouteGuard({ authenticated,user,element}){
     return <Navigate to='/auth'/>
     }
 
-    if(authenticated && user?.role !== 'admin' && (location.pathname.includes('admin') || location.pathname.includes("/auth"))){
+    if(authenticated && user?.role !== 'instructor' && (location.pathname.includes('instructor') || location.pathname.includes("/auth"))){
         return <Navigate to= "/home"/>
     }
-    if(authenticated && user.role === 'admin' && !location.pathname.includes('admin')){
-        return <Navigate to = "/admin"/>
+    if(authenticated && user.role === 'instructor' && !location.pathname.includes('instructor')){
+        return <Navigate to = "/instructor"/>
     }
 
     return <Fragment>{element}</Fragment>
